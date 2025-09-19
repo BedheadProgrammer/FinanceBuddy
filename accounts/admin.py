0dcf69190f898652551b0de189bddf980b658301
AndrewCart5 <acart2001@gmail.com>
2025-09-19 11:49:59 -0400

    Updating DB

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,8 +3,5 @@ from django.contrib import admin
 # Register your models here.
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
 
-@admin.register(User)
-class CustomUserAdmin(UserAdmin):
-    pass
+
