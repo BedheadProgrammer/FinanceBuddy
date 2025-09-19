92c30b78279f27a6f9bd25c25e6b5810a9786219
AndrewCart5 <acart2001@gmail.com>
2025-09-19 11:03:28 -0400

    Andrews first Commit

--- accounts/models.py
+++ accounts/models.py
@@ -2,12 +2,4 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-class User(AbstractUser):
-    """
-    Extend now so you're future-proof.
-    """
-    email = models.EmailField(unique=True)
-
-    def __str__(self):
-        return self.username or self.email
-
+# Create your models here.
