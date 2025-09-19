0cf1dcf372ea287a45da2b9d985a36350d49f954
AndrewCart5 <acart2001@gmail.com>
2025-09-19 11:07:15 -0400

    Adding DB tables

--- accounts/models.py
+++ accounts/models.py
@@ -1,5 +1,49 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-from django.db import models
+
+
+
 
 # Create your models here.
+class UserAccounts(models.Model):
+    Username = models.CharField(primary_key=True, max_length=150)
+    HashedPassword = models.CharField(max_length=255)
+    Date_Created = models.DateTimeField(auto_now_add=True)
+
+class AuditLog(models.Model):
+    id = models.AutoField(primary_key=True)
+    Date_Created = models.DateTimeField(auto_now_add=True)
+    ACTION_CHOICES = [
+        ("Login"),
+        ("Logout"),
+    ]
+    Action = models.CharField(max_length=10, choices=ACTION_CHOICES)
+    Username = models.ForeignKey(UserAccounts, to_field="Username", on_delete=models.CASCADE)
+
+class SavedRuns(models.Model):
+    id = models.AutoField(primary_key=True)
+    Date_Created = models.DateTimeField(auto_now_add=True)
+    SpotPrice = models.FloatField()
+    StrikePrice = models.FloatField()
+    TimeToExpiry = models.FloatField()
+    RiskFreeRate = models.FloatField()
+    DividendYield = models.FloatField()
+    Volatility = models.FloatField()
+    Price = models.FloatField()
+    Rho = models.FloatField()
+    Gamma = models.FloatField()
+    OptionType = models.CharField(max_length=4, choices=[("Call", "Call"), ("Put", "Put")])
+    ImpliedVolatility = models.FloatField()
+    Theta = models.FloatField()
+    Vega = models.FloatField()
+    Delta = models.FloatField()
+    Username = models.ForeignKey(UserAccounts, to_field="Username", on_delete=models.CASCADE)
+
+class OPENAIAPIRESPONSE(models.Model):
+    id = models.AutoField(primary_key=True)
+    Date_Added = models.DateTimeField(auto_now_add=True)
+    UserQuestion = models.TextField()
+    APIResponse = models.TextField()
+    SavedRun = models.ForeignKey(SavedRuns, to_field="id", on_delete=models.CASCADE)
+    Username = models.ForeignKey(UserAccounts, to_field="Username", on_delete=models.CASCADE)
+
+
